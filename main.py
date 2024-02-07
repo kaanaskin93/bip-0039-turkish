3d666470aedf91fc4150e79936a00160b500cba8
efecini-plentific <efe.cini@plentific.com>
2024-02-08 01:35:23 +0300

    emir&kaan added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from util import sort, writeToFile, readFromTXTFileAndGenerateArray, checkElementsInArray
 
-# checkElementsInArray(readFromTXTFileAndGenerateArray('v12_emirv1added_emirv2added_sorted_checked.txt'))
-# writeToFile(sort(readFromTXTFileAndGenerateArray('emir_v3.txt')))
+checkElementsInArray(readFromTXTFileAndGenerateArray('emir_v4.txt'))
+# writeToFile(sort(readFromTXTFileAndGenerateArray('emir_v5.txt')))
